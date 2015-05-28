ca37ffbe75c5c87baddb662bd30ac19f3f3a35d5
rurtubiac <randy.urtubia@inacapmail.cl>
2015-05-28 19:00:24 -0300

    Initial commit

---
+++ variables.py
@@ -0,0 +1,36 @@
+"""
+Multi line comment:
+variables in python must start with a letter,
+then it can have numbers or undersocres
+"""
+
+#   string variable
+name = 'rurtubia'
+
+print('my name is ' + name)
+
+# There are five main data types in python: numbers, strings, lists, tuples and dictionaries
+# number variables
+
+cinco = 5
+dos = 2
+
+# There are seven arithmetic operators
+
+#   +  addition
+print(cinco+dos)
+#   -  subtraction
+print(cinco-dos)
+#   *  multiplication
+print(cinco*dos)
+#   /  division
+print(cinco/dos)
+#   %  modulus (returns the division remainder)
+print(cinco%dos)
+#   ** exponential calculation
+print(cinco**dos)
+#   // floor division (performs a division and discards the remainder - rounds down the value)
+print(cinco//dos)
+
+#   Order of operations
+
